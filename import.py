02d2eee5f4a167e820771dfb120cd7787e156bc1
Luke Blaney <git@lukeblaney.co.uk>
2020-04-20 21:46:46 +0100

    Use lockfile to stop multiple runs concurrently
    Checks the pid against /proc/$pid on the filesystem, which isn't foolproof, but should be sufficient for most cases

--- import.py
+++ import.py
@@ -3,6 +3,7 @@
 import os, sys
 import requests, taglib, acoustid, json
 
+## Make sure required environment varibles are set
 if not os.environ.get("MEDIA_DIRECTORY"):
 	sys.exit("\033[91mMEDIA_DIRECTORY not set\033[0m")
 dirpath = os.environ.get("MEDIA_DIRECTORY")
@@ -12,6 +13,20 @@ mediaprefix = os.environ.get("MEDIA_PREFIX")
 if not os.environ.get("MEDIA_API"):
 	sys.exit("\033[91mMEDIA_API not set\033[0m")
 apiurl = os.environ.get("MEDIA_API")
+
+
+## Make sure only running once at a time, using lockfile
+try:
+	lockfile = open("import.lock", "r")
+	pid = lockfile.read()
+	lockfile.close()
+	if os.path.exists("/proc/"+pid+"/"): # Not fullproof as pids can be reused
+		sys.exit("\033[91mImport already running (pid "+pid+")\033[0m")
+except FileNotFoundError:
+	pass
+lockfile = open("import.lock", "w")
+lockfile.write(str(os.getpid()))
+lockfile.close()
 
 verbose = False
 
@@ -66,3 +81,5 @@ for root, dirs, files in os.walk(dirpath):
 				print(error, file=sys.stderr)
 		except Exception as error:
 			print (type(error).__name__, error, file=sys.stderr)
+
+os.remove("import.lock")
