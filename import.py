e2ad84a10a0a94f81b8001ee35d73365a7cf3e82
Luke Blaney <git@lukeblaney.co.uk>
2021-04-11 16:23:59 +0100

    make log lines a bit more readable

--- import.py
+++ import.py
@@ -28,11 +28,20 @@ lockfile = open("import.lock", "w")
 lockfile.write(str(os.getpid()))
 lockfile.close()
 
+verbose = False
+
+def log(message, error=False, debug=False):
+	if (debug and not verbose):
+		return
+	if error:
+		print("\033[91m** Error ** "+str(message)+"\033[0m", file=sys.stderr)
+	else:
+		print ("\033[92m"+str(message)+"\033[0m")
+
 loganne_result = requests.post("https://loganne.l42.eu/events", json={"source":"lucos_media_import","type":"import","humanReadable":"Scanning for new tracks to include in media library","dir": dirpath}, allow_redirects=False)
 if not loganne_result:
-	print ("\033[91m** Error ** Call to Loganne failed with "+str(loganne_result.status_code)+" response: " +  loganne_result.text + "\033[0m")
+	log("Call to Loganne failed with "+str(loganne_result.status_code)+" response: " +  loganne_result.text.rstrip(), error=True)
 
-verbose = False
 errorCount = 0
 
 for root, dirs, files in os.walk(dirpath):
@@ -62,51 +71,45 @@ for root, dirs, files in os.walk(dirpath):
 
 			duration, fingerprint = acoustid.fingerprint_file(path, maxlength=60)
 			if fingerprint.decode('UTF-8') in ["AQAAAA", "AQAAAQkz9UsCAQ"]:
-				if verbose:
-					print("Skipping empty track", trackurl)
+				log("Skipping empty track " + trackurl, debug=True)
 				continue
 			if duration < 1:
-				print("\033[91m** Error ** Track with duration less than 1 second ", trackurl, "\033[0m")
+				log("Track with duration less than 1 second " + trackurl, error=True)
 				errorCount += 1
 				continue
-			if verbose:
-				print (trackurl, tags, duration, fingerprint.decode('UTF-8'))
-			else:
-				print (trackurl)
 			trackdata = {
 				"fingerprint": fingerprint.decode('UTF-8'),
 				"duration": int(duration),
 				"tags": tags,
 			}
+			log(trackurl)
+			log(str(trackdata), debug=True)
 			trackresult = requests.put(apiurl+"/tracks", params={"url": trackurl}, data=json.dumps(trackdata), allow_redirects=False, headers={"If-None-Match": "*"})
 			if trackresult.ok:
-				if verbose:
-					print ("\033[92mTrack Updated: " +  trackresult.text + "\033[0m")
+				log("Track Updated: " +  trackresult.text.rstrip(), debug=True)
 			else:
-				print ("\033[91m** Error ** HTTP Status code "+str(trackresult.status_code)+" returned by API: " +  trackresult.text + "[fingerprint: " + fingerprint.decode('UTF-8') + " ]\033[0m")
+				log("HTTP Status code "+str(trackresult.status_code)+" returned by API: " +  trackresult.text.rstrip() + "[fingerprint: " + fingerprint.decode('UTF-8') + " ]", error=True)
 				errorCount += 1
-
 		except OSError as error:
-			if verbose:
-				print(error, file=sys.stderr)
+			log(error, error=True, debug=True)
 		except Exception as error:
-			print ("\033[91m", type(error).__name__, error, name, "\033[0m", file=sys.stderr)
+			log(type(error).__name__ + " " + str(error) + " " + name, error=True)
 			errorCount += 1
 
 # Save the current time as a global in the media API
 summaryresult = requests.put(apiurl+"/globals/latest_import-timestamp", data=datetime.datetime.utcnow().isoformat().encode('utf-8'), allow_redirects=False)
 if summaryresult.ok:
-	print ("\033[92mLast import timestamp updated: " +  summaryresult.text + "\033[0m")
+	log("Last import timestamp updated: " +  summaryresult.text.rstrip())
 else:
-	print ("\033[91m** Error ** HTTP Status code "+str(summaryresult.status_code)+" returned by API: " +  summaryresult.text + "\033[0m")
+	log("HTTP Status code "+str(summaryresult.status_code)+" returned by API: " +  summaryresult.text.rstrip(), error=True)
 	errorCount += 1
 
 # Save the number of errors as a global in the media API
 summaryresult = requests.put(apiurl+"/globals/latest_import-errors", data=str(errorCount), allow_redirects=False)
 if summaryresult.ok:
-	print ("\033[92mLast import error count updated: " +  summaryresult.text + "\033[0m")
+	log("Last import error count updated: " +  summaryresult.text.rstrip())
 else:
-	print ("\033[91m** Error ** HTTP Status code "+str(summaryresult.status_code)+" returned by API: " +  summaryresult.text + "\033[0m")
+	log("HTTP Status code "+str(summaryresult.status_code)+" returned by API: " +  summaryresult.text.rstrip(), error=True)
 	errorCount += 1
 
 os.remove("import.lock")
