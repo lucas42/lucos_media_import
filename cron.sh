2f91d7d1267f681cc32242c5b7c9e0a1e83c942d
Luke Blaney <git@lukeblaney.co.uk>
2022-05-08 21:10:23 +0100

    quote values in .env to support space in "ceol srl" directory

--- cron.sh
+++ cron.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
-printenv > .env
+
+# printenv doesn't quote values, which is a problem if one contains a space
+# So do some hacky regexes to quote stuff
+printenv | sed 's/"/\\"/g' | sed 's/=/="/g' | sed 's/$/"/g' > .env
 [ -p /var/log/cron.log ] || mkfifo /var/log/cron.log
 service cron start
 cat <> /var/log/cron.log
