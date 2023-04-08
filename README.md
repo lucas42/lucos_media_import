d1847a047c626670a40cab145de6135535e9ef57
Luke Blaney <git@lukeblaney.co.uk>
2023-04-08 12:10:15 +0100

    add unit tests for the scan_file logic

--- README.md
+++ README.md
@@ -31,6 +31,11 @@ Run `pipenv install` to setup
 
 `pipenv run python import.py`
 
+## Testing
+
+Doesn't use a proper testing framework.  However, run
+`pipenv run python src/test.py`
+which will check various calls to the scan_file() function, relying on files in the `test_tracks` directory
 
 ## Environment Variables
 For local development, these should be stored in a .env file
@@ -45,4 +50,6 @@ For local development, these should be stored in a .env file
 * `src` directory holds the python source code
   - `logic.py` holds the logic for evaluating an audio track on the filesystem and adding an appropriate entry in the metadata API.
   - `import.py` is script which iterates through all files in the _MEDIA_DIRECTORY_ and imports them to the API.
+  - `test.py` does some simple checks on the logic in `src/logic.py`.
 * `startup.sh` ensures the cron daemon is running with the right environment set up and sharing its logs in a way that get surfaced to Docker
+* `test_tracks` a collection of audio tracks used for testing.  Not included in the final docker image
---
+++ src/test.py
@@ -0,0 +1,73 @@
+#! /usr/local/bin/python3
+import os, datetime
+
+# Set the media prefix to a known value for testing
+os.environ["MEDIA_PREFIX"] = "http://example.org/media_library/"
+
+# Unit under test
+from logic import scan_file
+
+testcases = [
+	{
+		'comment': "Normal Track",
+		'path': "test_tracks/A Testing Day.mp3",
+		'expected_data': {
+			'duration': 4,
+			'tags': {
+				'title': 'A Testing Day',
+				'artist': 'Computerface',
+				'added': '2023-04-08T11:00:31.234752',
+			},
+			'url': "http://example.org/media_library/test_tracks/A Testing Day.mp3",
+		}
+	},
+	{
+		'comment': "Short Track is skipped",
+		'path': "test_tracks/Short.mp3",
+		'expected_error': "Empty Track",
+	},
+	{
+		'comment': "Empty Track is skipped",
+		'path': "test_tracks/Empty.mp3",
+		'expected_error': "Empty Track",
+	},
+	{
+		'comment': "Track without title defaults to filename",
+		'path': "test_tracks/No Title.mp3",
+		'expected_data': {
+			'duration': 4,
+			'tags': {
+				'title': 'No Title',
+				'artist': 'Computerface',
+				'added': '2023-04-08T11:55:01.620345',
+			},
+			'url': "http://example.org/media_library/test_tracks/No Title.mp3",
+		}
+	},
+]
+failures = 0
+
+for case in testcases:
+	try:
+		(actual_fingerprint, actual_data) = scan_file(case["path"])
+		if "expected_error" in case:
+			print("\033[91mFailed\033[0m \"" + case['comment'] + "\".  No error raised.")
+			print(actual_data["duration"])
+			failures += 1
+			continue
+		if case["expected_data"] != actual_data:
+			print("\033[91mFailed\033[0m \"" + case['comment'] + "\".  Returned \033[91m" + str(actual_data) + "\033[0m, expected " + str(case['expected_data']))
+			failures += 1
+	except Exception as error:
+		if "expected_error" not in case:
+			print("\033[91mFailed\033[0m \"" + case['comment'] + "\".  Unexpected error raised: \033[91m"+type(error).__name__ + " " + str(error) + "\033[0m")
+			failures += 1
+		elif case["expected_error"] != str(error):
+			print("\033[91mFailed\033[0m \"" + case['comment'] + "\".  Mismatched error message: Returned \033[91m" + str(error) + "\033[0m, expected " + str(case['expected_error']))
+			failures += 1
+
+if (failures > 0):
+	print("\033[91m"+str(failures) + " failures\033[0m in " + str(len(testcases)) + " cases.")
+	exit(1)
+else:
+	print("All " + str(len(testcases)) + " cases passed.")
