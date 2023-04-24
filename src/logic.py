220b3b63cbffa58526dcc75b21c9bf81bf1e610c
Luke Blaney <git@lukeblaney.co.uk>
2023-04-24 22:28:03 +0100

    remove unneccessary try/except block

--- src/logic.py
+++ src/logic.py
@@ -11,44 +11,41 @@ mediaprefix = os.environ.get("MEDIA_PREFIX")
 verbose = False
 
 def scan_file(path):
+	trackurl = mediaprefix + urllib.parse.quote(path, safe='/ ')
 	try:
-		trackurl = mediaprefix + urllib.parse.quote(path, safe='/ ')
-		try:
-			filemetadata = taglib.File(path)
-		except OSError:
-			return (None, None)
-		tags = {}
-		for key, values in filemetadata.tags.items():
-			if len(values) < 1:
-				continue
-			key = key.lower()
-			value = " & ".join(values)
-			if key not in ["title", "album", "artist", "year", "genre", "comment", "lyrics"]:
-				continue
-			tags[key] = value
+		filemetadata = taglib.File(path)
+	except OSError:
+		return (None, None)
+	tags = {}
+	for key, values in filemetadata.tags.items():
+		if len(values) < 1:
+			continue
+		key = key.lower()
+		value = " & ".join(values)
+		if key not in ["title", "album", "artist", "year", "genre", "comment", "lyrics"]:
+			continue
+		tags[key] = value
 
-		# If there's no title in the ID3 tags, default to filename (ignoring extension)
-		if "title" not in tags:
-			tags["title"] = path.split("/")[-1].rsplit(".", 1)[0]
+	# If there's no title in the ID3 tags, default to filename (ignoring extension)
+	if "title" not in tags:
+		tags["title"] = path.split("/")[-1].rsplit(".", 1)[0]
 
-		# Many tracks from compilation albums are tagged with an artist of "Various" - this is really unhelpful in a library context, so ignore this tag
-		if "artist" in tags and tags["artist"] == "Various":
-			del tags["artist"]
+	# Many tracks from compilation albums are tagged with an artist of "Various" - this is really unhelpful in a library context, so ignore this tag
+	if "artist" in tags and tags["artist"] == "Various":
+		del tags["artist"]
 
-		duration, fingerprint = acoustid.fingerprint_file(path, maxlength=60)
+	duration, fingerprint = acoustid.fingerprint_file(path, maxlength=60)
 
-		if fingerprint.decode('UTF-8') in ["AQAAAA", "AQAAAQkz9UsCAQ"]:
-			raise Exception("Empty Track")
-		if duration < 1:
-			raise Exception("Track with duration less than 1 second")
-		trackdata = {
-			"duration": int(duration),
-			"tags": tags,
-			"url": trackurl,
-		}
-		return (fingerprint, trackdata)
-	except Exception as error:
-		raise error
+	if fingerprint.decode('UTF-8') in ["AQAAAA", "AQAAAQkz9UsCAQ"]:
+		raise Exception("Empty Track")
+	if duration < 1:
+		raise Exception("Track with duration less than 1 second")
+	trackdata = {
+		"duration": int(duration),
+		"tags": tags,
+		"url": trackurl,
+	}
+	return (fingerprint, trackdata)
 
 def scan_insert_file(path):
 	(fingerprint, trackdata) = scan_file(path)
