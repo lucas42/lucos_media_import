e9fcd822b9ed39301627d933104fc1a3a7f01282
Luke Blaney <git@lukeblaney.co.uk>
2025-07-30 20:57:44 +0100

    make loganne endpoint configurable with environment variable LOGANNE_ENDPOINT

--- src/loganne.py
+++ src/loganne.py
@@ -1,8 +1,12 @@
-import requests
+import os, sys, requests
 from datetime import datetime
+
+if not os.environ.get("LOGANNE_ENDPOINT"):
+	sys.exit("\033[91mLOGANNE_ENDPOINT not set\033[0m")
+loganne_endpoint = os.environ.get("LOGANNE_ENDPOINT")
 
 def loganneRequest(data):
 	data["source"] = "lucos_media_import"
-	loganne_reponse = requests.post('https://loganne.l42.eu/events', json=data);
+	loganne_reponse = requests.post(loganne_endpoint, json=data);
 	if loganne_reponse.status_code != 202:
 		print ("\033[91m ["+datetime.now().isoformat()+"] ** Error ** Call to Loganne failed with "+str(loganne_response.status_code)+" response: " +  loganne_response.text + "\033[0m")
