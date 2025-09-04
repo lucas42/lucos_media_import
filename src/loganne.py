7cbce17ef2c072f072065d091b8f7e46787e0ad6
Luke Blaney <git@lukeblaney.co.uk>
2025-09-04 19:41:29 +0100

    handle loganne errors more gracefully

--- src/loganne.py
+++ src/loganne.py
@@ -7,6 +7,8 @@ loganne_endpoint = os.environ.get("LOGANNE_ENDPOINT")
 
 def loganneRequest(data):
 	data["source"] = "lucos_media_import"
-	loganne_reponse = requests.post(loganne_endpoint, json=data, headers={'User-Agent': "lucos_media_import"});
-	if loganne_reponse.status_code != 202:
-		print ("\033[91m ["+datetime.now().isoformat()+"] ** Error ** Call to Loganne failed with "+str(loganne_response.status_code)+" response: " +  loganne_response.text + "\033[0m")
+	try:
+		loganne_reponse = requests.post(loganne_endpoint, json=data, headers={'User-Agent': "lucos_media_import"})
+		loganne_reponse.raise_for_status()
+	except Exception as error:
+		print("\033[91m [{}] ** Error from Loganne: {}\033[0m".format(datetime.now().isoformat(),error))
