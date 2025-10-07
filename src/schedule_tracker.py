494efc30fa53ca42db2ed93ab4398f28378c2e45
Luke Blaney <git@lukeblaney.co.uk>
2025-10-08 00:05:48 +0100

    Make SCHEDULE_TRACKER_ENDPOINT an environment variable, rather than hardcoded

--- src/schedule_tracker.py
+++ src/schedule_tracker.py
@@ -1,5 +1,9 @@
-import json, requests
+import json, requests, os
 from datetime import datetime
+
+SCHEDULE_TRACKER_ENDPOINT = os.environ.get('SCHEDULE_TRACKER_ENDPOINT')
+if not SCHEDULE_TRACKER_ENDPOINT:
+	exit("SCHEDULE_TRACKER_ENDPOINT environment variable not set - needs to be the URL of a running lucos_contacts instance.")
 
 # Inform the schedule tracker that the job is complete
 def updateScheduleTracker(success=True, message=None, system="lucos_media_import", frequency=1):
@@ -10,7 +14,7 @@ def updateScheduleTracker(success=True, message=None, system="lucos_media_import
 		"message": message,
 	}
 	try:
-		schedule_tracker_response = requests.post('https://schedule-tracker.l42.eu/report-status', json=payload);
+		schedule_tracker_response = requests.post(SCHEDULE_TRACKER_ENDPOINT, json=payload);
 		schedule_tracker_response.raise_for_status()
 	except Exception as error:
 		print("\033[91m ["+datetime.now().isoformat()+"] ** Error ** Call to schedule-tracker failed.  "+type(error).__name__ + " " + str(error) + "\033[0m")
